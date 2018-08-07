82bb2aa47cee87fade7fc7a1e85eef731fd0743f
Divyaranjan Behera <divyaranjanb@mindfiresolutions.com>
2018-08-07 11:36:06 +0530

    normal object to prototype class

--- extension.js
+++ extension.js
@@ -12,3 +12,26 @@ HTMLDivElement.prototype.toggleModal = function (toggle) {
     modal.style.display = (display === 'none' || display === '') ? 'block' : 'none';
     return true;
 }
+
+
+//###Object extensions
+Object.prototype.isEmpty = function() {
+    for(var key in this) {
+        if(this.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
+
+Object.prototype.toClass = function(classType){
+	if(!classType || typeof(classType) != 'function' || !classType.prototype.constructor){ return new Object();}
+	var objClass = new classType();
+	if(this.isEmpty()){return objClass;}
+	for(var eachItem in objClass)
+    {
+        objClass[eachItem] = this[eachItem] || objClass[eachItem];
+    }
+	return objClass;
+}
+
+
